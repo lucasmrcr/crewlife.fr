32b9763dc72793f80104a52563486e1742fd2985
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-04-23 16:00:28 +0200

    add new icons to fontawesome

--- src/vendor/fontawesome.ts
+++ src/vendor/fontawesome.ts
@@ -1,6 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faPlay } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
-    faSearch
+    faSearch,
+    faPlay
 );
