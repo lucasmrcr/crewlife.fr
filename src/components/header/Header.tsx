22e1e73c9c9f0c0eeb25f48867e7026c7c4b2a6e
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-04-23 16:02:14 +0200

    implements join button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import Navigation from './components/Navigation'
 import background from '../../assets/images/background.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const Header: React.FC =  () => (
     <header className="page-header" style={{backgroundImage: `url(${background})`}}>
         <Navigation />
 
         <section className="welcome">
-            <h1>Bienvenue sur le serveur <span>CrewLife</span> !</h1>
-            <h4>Serveur <span>Arma 3</span> Altis Life roleplay & pvp.</h4>
+            <div className="welcome--description">
+                <h1>Bienvenue sur le serveur <span>CrewLife</span> !</h1>
+                <h4>Serveur <span>Arma 3</span> Altis Life roleplay & pvp.</h4>
+            </div>
+            <div className="welcome--play-button">
+                <button>
+                    <FontAwesomeIcon icon={['fas', 'play']} />
+                    Rejoignez les 125 connectés
+                </button>
+            </div>
         </section>
     </header>
 )
